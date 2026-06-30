087de5f9319e45f45f635d50fdfdfd0d8c332542
aeinfo515-sys <ae.info.515@gmail.com>
2026-06-30 16:40:36 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('v8 loaded');
+
+fetch('data/schedule.json')
+.then(r=>r.json())
+.then(data=>{
+  const el=document.getElementById('schedule');
+
+  if(!data.length){
+    el.innerHTML='本日の出演はありません';
+    return;
+  }
+
+  el.innerHTML = data.map(i=>`
+    <div style="padding:8px 0;border-bottom:1px solid #eee">
+      <b>${i.title}</b><br>
+      <small>${i.time} / ${i.date}</small>
+    </div>
+  `).join('');
+});
