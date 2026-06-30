1940496b6ca5df819eda05b9fd8c30f0b118d37b
aeinfo515-sys <ae.info.515@gmail.com>
2026-06-30 16:34:07 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-
-fetch('data/schedule.json')
-.then(r=>r.json())
-.then(data=>{
-  const el=document.getElementById('schedule');
-  if(!el) return;
-  el.innerHTML=data.map(d=>`<div class='card'>${d.date} ${d.title} ${d.time}</div>`).join('');
-});
-
-const input=document.getElementById('search');
-if(input){
- input.addEventListener('input',e=>{
-  console.log(e.target.value);
- });
-}
+console.log('v8 loaded');
